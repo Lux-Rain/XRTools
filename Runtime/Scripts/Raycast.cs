16e3648c27333b293ed3d0855b1844db318dfbdd
Teo Diaz <diaz.teo@outlook.com>
2021-01-18 10:18:04 +0100

    Add Raycast
    Add Raycast Once
    Add Raycast with delay

---
+++ Runtime/Scripts/Raycast.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Raycast : MonoBehaviour
+{
+    protected float t;
+    protected bool raycastActive;
+
+    [Header("Raycast")]
+    [SerializeField]
+    protected Transform raycastStart;
+    [SerializeField]
+    protected float raycastLenght;
+    [SerializeField]
+    protected LayerMask raycastLayer;
+
+    [Header("Delay")]
+    [SerializeField]
+    protected float raycastDelay;
+
+    [Header("Event")]
+    [SerializeField]
+    protected string broadcastMessage;
+    [SerializeField]
+    protected UnityEvent OnShootRaycast;
+    [SerializeField]
+    protected UnityEvent OnHitTarget;
+
+    /// <summary>
+    /// Send one Raycast
+    /// </summary>
+    public void SendRaycast()
+    {
+        OnShootRaycast.Invoke();
+        if(Physics.Raycast(raycastStart.position, raycastStart.forward, out RaycastHit hit, raycastLenght, raycastLayer))
+        {
+            if(broadcastMessage != "")
+            {
+                hit.transform.gameObject.BroadcastMessage(broadcastMessage);
+            }
+            OnHitTarget.Invoke();
+        }
+    }
+
+    /// <summary>
+    /// Send Multiple Raycast
+    /// </summary>
+    public void ActivateRaycast()
+    {
+        raycastActive = true;
+        t = 0;
+    }
+
+    public void DeactivateRaycast()
+    {
+        raycastActive = false;
+    }
+
+    private void Update()
+    {
+        if (raycastActive)
+        {
+            t += Time.deltaTime;
+            if(t >= raycastDelay)
+            {
+                t = 0;
+                SendRaycast();
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(raycastStart.position, raycastStart.forward * raycastLenght);
+    }
+}
---
+++ Runtime/Scripts/Target.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField]
+    protected UnityEvent OnHit;
+
+    virtual public void Hit()
+    {
+        OnHit.Invoke();
+    }
+}
